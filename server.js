3c45166b1b4a6183c03dbb624b8e7b451a6531c1
ADITI <aditi23github@gmail.com>
2026-04-20 18:54:47 +0530

    Add dotenv configuration and refactor server start

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
+import dotenv from "dotenv";
 import { connectDB } from "./src/config/db.js";
 import app from "./app.js";
 
+dotenv.config();
+
 const PORT = process.env.PORT || 5500;
 
-connectDB();
+const startServer = async () => {
+  try {
+    await connectDB();
 
-app.listen(PORT,()=>{
-    console.log(`Server listening on PORT :${PORT}`);
-})
+    app.listen(PORT, () => {
+      console.log(`Server listening on PORT: ${PORT}`);
+    });
+
+  } catch (err) {
+    console.error("Failed to start server:", err.message);
+    process.exit(1);
+  }
+};
+
+startServer();
